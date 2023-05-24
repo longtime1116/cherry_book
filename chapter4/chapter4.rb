58946f4b0c0e96cbf4f21ae2d17e8b2124c96e7f
longtime1116 <longtime1116@gmail.com>
2023-05-24 15:53:05 +0900

    chapter4 done

--- chapter4/chapter4.rb
+++ chapter4/chapter4.rb
@@ -15,3 +15,44 @@ def liquid?(temperature)
 end
 
 p (1..10).step(2).map { |n| n + 1 }
+
+# a,c,d以外の要素をbに
+a, *b, c, d = 1, 2, 3, 4, 5
+p b
+
+def hoge(_a, *b)
+  p b
+  p b.class #=> 配列で受け取っている
+end
+hoge(1, 2, 3)
+
+# 展開する記号としての*(splat演算子)
+a = []
+b = [2, 3]
+a.push(1)
+a.push(*b) #=> a.push(2,3) と同じ意味。
+p a
+
+a = [1, 2, 3]
+p [a]
+p [*a]
+
+# 2.7から使える、番号指定パラメータ。引数を_nで取得できる
+p %w[1 20 300].map { _1.rjust(3, '0') }
+
+# 終端を持たないRangeオブジェクト
+(10..).each do |i|
+  break if i > 15
+
+  p i
+end
+
+# redo は繰り返し処理をやり直す
+foods = %w[tomato apple banana]
+foods.each do |food|
+  print "Do you like #{food}? => "
+  answer = %w[yes no].sample
+  puts answer
+
+  redo unless answer == 'yes'
+end
--- chapter4/lib/rgb.rb
+++ chapter4/lib/rgb.rb
@@ -5,6 +5,7 @@ def to_hex(r, g, b)
 end
 
 def to_ints(str)
+  # str[1..2]とか、str[1,1]とかで抜き出しても良いが、ここでは正規表現。
   # 連続する2文字のアルファベット、数字、またはアンダースコアにマッチ
   str.scan(/\w\w/).map(&:hex)
 end
